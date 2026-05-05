d88c652f8873e9d825777e56369cbe2628c0776b
ghulamali17 <ghulamali47092@gmail.com>
2026-05-05 21:45:41 +0500

    Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BrowseModule } from './browse/browse.module';
@@ -9,10 +9,20 @@ import { StudentController } from './student/student.controller';
 import { CustomerModule } from './customer/customer.module';
 import { MynameController } from './myname/myname.controller';
 import { ExceptionController } from './exception/exception.controller';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [BrowseModule, ProductModule, CategoryModule, CustomerModule],
-  controllers: [AppController, StudentController, MynameController, ExceptionController],
+  controllers: [
+    AppController,
+    StudentController,
+    MynameController,
+    ExceptionController,
+  ],
   providers: [AppService, StudentService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
